f2e3c89c895c3130753dd91658c7bdc78a259775
Samantha Mae Ramirez <152503241+smramirez4@users.noreply.github.com>
2024-02-14 01:36:16 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+function randomNumber(min,max){
+    return Math.floor(Math.random() * (max-min +1)) + min;
+}
+
+const b = document.querySelector('.button--no');
+b.addEventListener("mouseover", moveHover);
+
+function moveHover() {
+  const i = Math.floor(Math.random() * 500) + 1;
+  const j = Math.floor(Math.random() * 500) + 1;
+
+  b.style.left = i + "px";
+  b.style.top = j + "px";
+}
